505ca0156f78228ae9467ebd7ae14cfade610cdd
Celia Garrido <celiagarridoherrera@gmail.com>
2025-02-02 02:07:48 +0100

    create method for TablaMultiplicar

--- src/main/java/dev/celia/tablamultiplicar/TablaMultiplicar.java
+++ src/main/java/dev/celia/tablamultiplicar/TablaMultiplicar.java
@@ -1,5 +1,17 @@
 package dev.celia.tablamultiplicar;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TablaMultiplicar {
+
+    public static List<String> generarTablaMultiplicar(int n) {
+        List<String> tabla = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            tabla.add(n + " x " + i + "=" + (n * i));
+        }
+        return tabla;
+    }
+
 
 }
